a6ef17b519805db10a2c9a460ab7e685575fd140
e2eteam <@norize49>
2023-12-29 20:03:16 +0200

    First commit

---
+++ MAIN.py
@@ -0,0 +1,46 @@
+# print("Hello World")
+# print()
+# print("Hello\nAndrey")
+# print("Hello\n\t\"Andrey\"")
+# print("Hello\tAndrey")
+# print("Hello\nAndrey")
+# print("Hello\nAndrey")
+
+
+# number = 20
+# print(number)
+# print(type(number))
+# number = 40.5
+# print(number)
+# print(type(number))
+# number = "Liza"
+# print(number)
+# print(type(number))
+# number = True
+# print(number)
+# print(type(number))
+
+# userName1 = "Liza"
+# print(userName1)
+# user_name = "Andrey"
+# print(user_name)
+
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+#
+# # print("Name: ", name, "Age:", age)
+#
+# print("Name: " + name, "Age:" + str(age))
+#
+# # print(f"Name: {name} name, age: {age}")
+#
+# print(age + 1)
+
+
+
+
+print(2+3)
+print(2 ** 3)
+print(2 // 3)
+print(2 % 3)
---
+++ home.py
@@ -0,0 +1,41 @@
+# Задача 1
+# Введення три цифри від користувача
+# number1 = float(input("Введіть перше число: "))
+# number2 = float(input("Введіть друге число: "))
+# number3 = float(input("Введіть третє число: "))
+#
+# # Обчислення суми та добутку чисел
+# suma = number1 + number2 + number3
+# dobutok = number1 * number2 * number3
+#
+# # Виведення результату на екран
+# print(f"Сума введених чисел: {suma}")
+# print(f"Добуток введених чисел: {dobutok}")
+
+# Задача 2
+# def obchyslennia_ploshchi_romba(ac, bd):
+#     # Обчислення площі ромба
+#     ploshcha = (ac * bd) / 2
+#     return ploshcha
+#
+# # Введення довжин діагоналей від користувача
+# ac = float(input("Введіть довжину першої діагоналі: "))
+# bd = float(input("Введіть довжину другої діагоналі: "))
+#
+# # Обчислення та виведення площі ромба
+# ploshcha_romba = obchyslennia_ploshchi_romba(ac, bd)
+# print(f"Площа ромба з діагоналями {ac} і {bd} дорівнює: {ploshcha_romba}")
+
+# Доп задача 3
+# Задача 1
+# Введення три цифри від користувача
+# number1 = float(input("Введіть перше число: "))
+# number2 = float(input("Введіть друге число: "))
+# number3 = float(input("Введіть третє число: "))
+# number4 = float(input("Введіть четверте число: "))
+#
+# # Обчислення добутку чисел
+# dobutok = number1 * number2 * number3 * number4
+#
+# # Виведення результату на екран
+# print(f"Добуток введених чисел: {dobutok}")
