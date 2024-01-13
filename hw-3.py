af0869b2990e5ecdf9e371d74725fb4a5c11d976
e2eteam <@norize49>
2024-01-13 16:08:29 +0200

    homework 3

---
+++ hw-3.py
@@ -0,0 +1,59 @@
+# Завдання 1
+
+# day = int(input("Введіть номер дня тижня: "))
+
+# if day == 1:
+#     print("Понеділок")
+# elif day == 2:
+#     print("Вівторок")
+# elif day == 3:
+#     print("Середа")
+# elif day == 4:
+#     print("Четверг")
+# elif day == 5:
+#     print("П'ятниця")
+# elif day == 6:
+#     print("Субота")
+# elif day == 7:
+#     print("Неділя")
+# else:
+#     print("Невірний номер дня тижня")
+
+
+# Завдання 3
+
+# number1 = float(input("Введіть перше число: "))
+# number2 = float(input("Введіть друге число: "))
+#
+# print("Виберіть операцію:")
+# print("1. +")
+# print("2. -")
+# print("3. *")
+# print("4. /")
+#
+# oper = int(input("Введіть номер операції: "))
+#
+# if oper == 1:
+#     print(number1 + number2)
+# elif oper == 2:
+#     print(number1 - number2)
+# elif oper == 3:
+#     print(number1 * number2)
+# elif oper == 4:
+#     print(number1 / number2)
+# else:
+#     print("Невірний номер операції")
+
+
+# Завдання 2
+
+# number1 = float(input("Введіть перше число: "))
+# number2 = float(input("Введіть друге число: "))
+#
+# if number1 == number2:
+#     print("Числа рівні")
+# else:
+#     if number1 < number2:
+#         print(number1, number2)
+#     else:
+#         print(number2, number1)
